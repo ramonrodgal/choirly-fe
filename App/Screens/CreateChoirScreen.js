7208bcfee8a5e149be7b896f7fbdf8e804c99622
Serena Abbott <serena.abbott@hotmail.co.uk>
2021-12-07 16:33:14 +0000

    Start basic form for creating a choir

--- App/Screens/CreateChoirScreen.js
+++ App/Screens/CreateChoirScreen.js
@@ -1,12 +1,60 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { Text, View, TextInput, Button, Alert, StyleSheet } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
 export default function CreateChoirScreen() {
-    return (
-        <View>
-            <Text>Create Choir</Text>
-        </View>
-    )
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <View>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="firstName"
+      />
+      {errors.firstName && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="lastName"
+      />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: { alignItems: "center" },
+});
--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -1,52 +1,60 @@
 import React from "react";
-import { Button, StyleSheet, ImageBackground, Text, View, Image } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  ImageBackground,
+  Text,
+  View,
+  Image,
+} from "react-native";
 
 export default function Home({ navigation }) {
   return (
-    <ImageBackground 
-    style={styles.background}
-    source={{ uri: 'https://i.pinimg.com/originals/37/af/c1/37afc1f506c8276fa4fffb6e119ee054.jpg'}}>
-        <View style={styles.logoContainer}>
+    <ImageBackground
+      style={styles.background}
+      source={{
+        uri: "https://i.pinimg.com/originals/37/af/c1/37afc1f506c8276fa4fffb6e119ee054.jpg",
+      }}
+    >
+      <View style={styles.logoContainer}>
         <Text>Homepage</Text>
-        <Image style={styles.logo} source={require('../assets/choirly.png')} />
+        <Image style={styles.logo} source={require("../assets/choirly.png")} />
         <Button
-        title="Go to profile"
-        onPress={() => navigation.navigate("UserProfile")}
-      />
-        </View>
-        <View style={styles.loginButton}><Text>Login</Text></View>
-        <View style={styles.registerButton}><Text>Register Now!</Text></View>
+          title="Create choir"
+          onPress={() => navigation.navigate("CreateChoir")}
+        />
+      </View>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   background: {
-      flex: 1,
-      justifyContent: "flex-end",
-      alignItems: "center"
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   loginButton: {
-      width: '100%',
-      height: 70,
-      backgroundColor: 'rgb(151, 99, 31)',
-      alignItems: "center",
-      justifyContent: "center",
+    width: "100%",
+    height: 70,
+    backgroundColor: "rgb(151, 99, 31)",
+    alignItems: "center",
+    justifyContent: "center",
   },
   logo: {
-      width: 300,
-      height: 300,
+    width: 300,
+    height: 300,
   },
   logoContainer: {
-      position: "absolute",
-      top: 70,
-      alignItems: "center"
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
   },
   registerButton: {
-      width: '100%',
-      height: 70,
-      backgroundColor: 'rgb(170, 117, 47)',
-      alignItems: "center",
-      justifyContent: "center",
-  }
-})
+    width: "100%",
+    height: 70,
+    backgroundColor: "rgb(170, 117, 47)",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
