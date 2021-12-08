921e4c5e1909c1fecc2e458fd2b79110d4d306e9
Serena Abbott <serena.abbott@hotmail.co.uk>
2021-12-08 10:25:17 +0000

    Customises the create choir group form

--- App/Screens/CreateChoirScreen.js
+++ App/Screens/CreateChoirScreen.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Text, View, TextInput, Button, Alert, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  StyleSheet,
+  SafeAreaView,
+} from "react-native";
 import { useForm, Controller } from "react-hook-form";
 
 export default function CreateChoirScreen() {
@@ -9,14 +17,17 @@ export default function CreateChoirScreen() {
     formState: { errors },
   } = useForm({
     defaultValues: {
-      firstName: "",
-      lastName: "",
+      choirName: "",
+      location: "",
+      description: "",
+      avatarUrl: "",
     },
-  });
-  const onSubmit = (data) => console.log(data);
+  }); // all this is from useForm which is imported from react-hook-form
+  const onSubmit = (data) => console.log(data); // on submit the data is logged
 
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.label}>Choir name</Text>
       <Controller
         control={control}
         rules={{
@@ -25,36 +36,105 @@ export default function CreateChoirScreen() {
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             style={styles.input}
+            placeholder="Enter a choir group name here"
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
           />
         )}
-        name="firstName"
+        name="choirName"
       />
-      {errors.firstName && <Text>This is required.</Text>}
+      {errors.choirName && <Text>A choir name is required.</Text>}
 
+      <Text style={styles.label}>Location</Text>
       <Controller
         control={control}
         rules={{
-          maxLength: 100,
+          required: true,
         }}
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
             style={styles.input}
+            placeholder="Enter a location here"
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
           />
         )}
-        name="lastName"
+        name="location"
       />
+      {errors.location && <Text>A location is required.</Text>}
 
-      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-    </View>
+      <Text style={styles.label}>Description</Text>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder="Enter a description here"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="description"
+      />
+      {errors.description && <Text>A description is required.</Text>}
+
+      <Text style={styles.label}>Image URL</Text>
+      <Controller
+        control={control}
+        rules={{
+          required: false,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={styles.input}
+            placeholder="Enter an image url here"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="avatarUrl"
+      />
+      <View style={styles.button}>
+        <Button title="Create a choir group" onPress={handleSubmit(onSubmit)} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  input: { alignItems: "center" },
+  input: {
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 1,
+    height: 40,
+    padding: 10,
+    borderRadius: 4,
+  },
+  label: {
+    color: "black",
+    padding: 0,
+    margin: 2,
+    marginLeft: 0,
+    alignItems: "center",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    padding: 2,
+    backgroundColor: "white",
+  },
+  button: {
+    marginTop: 40,
+    color: "white",
+    height: 40,
+    backgroundColor: "beige",
+    borderRadius: 4,
+  },
 });
