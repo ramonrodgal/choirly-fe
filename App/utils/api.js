d6c5933a6d2872a2807489c05da9de4464d0e18a
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-12-12 15:21:07 +0000

    Create getUserByUsername util funtion

--- App/utils/api.js
+++ App/utils/api.js
@@ -1,26 +1,33 @@
 import axios from "axios";
 
 const choirApi = axios.create({
-    baseURL: "https://choirly.herokuapp.com/api"
-})
+  baseURL: "https://choirly.herokuapp.com/api",
+});
 
 export const getChoirs = (location) => {
-    if(location) return choirApi.get(`/choirs?location=${location}`).then((res) => {
-        return res.data.choirs
-    })
-    return choirApi.get("/choirs").then((res) => {
-        return res.data.choirs;
-    })
-}
+  if (location)
+    return choirApi.get(`/choirs?location=${location}`).then((res) => {
+      return res.data.choirs;
+    });
+  return choirApi.get("/choirs").then((res) => {
+    return res.data.choirs;
+  });
+};
 
 export const getChoirById = (choirId) => {
-    return choirApi.get(`/choirs/${choirId}`).then((res) => {
-        return res.data.choir;
-    })
-}
+  return choirApi.get(`/choirs/${choirId}`).then((res) => {
+    return res.data.choir;
+  });
+};
 
 export const getEventsByChoir = (choirId) => {
-    return choirApi.get(`/events/choir/${choirId}`).then((res) => {
-        return res.data.events;
-    })
-}
+  return choirApi.get(`/events/choir/${choirId}`).then((res) => {
+    return res.data.events;
+  });
+};
+
+export const getUserByUsername = (username) => {
+  return choirApi.get(`/users/${username}`).then((res) => {
+    return res.data.user;
+  });
+};
