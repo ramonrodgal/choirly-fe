45975df6dfddb70f0828fe009db00f5db13191f3
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-12-13 13:32:52 +0000

    Create handleAccept function

--- App/components/Notification.js
+++ App/components/Notification.js
@@ -5,26 +5,53 @@ import { updateNotificationById } from "../utils/api";
 export default function Notification({ notification }) {
   const [notificationObj, setNotificationObj] = useState(notification);
 
+  console.log(notificationObj);
+
   useEffect(() => {
     const body = {
       read: true,
     };
-    updateNotificationById(notification._id, body).then((notification) => {
-      setNotificationObj(notification);
-    });
+    updateNotificationById(notification._id, body)
+      .then((notification) => {
+        console.log(notification);
+        setNotificationObj(notification);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
   }, []);
 
-  if (notification.type === "join") {
-    return (
-      <View>
-        <Text>
-          {notificationObj.author} wants to join {notificationObj.choir}
-        </Text>
-        <Text>{Date(notificationObj.date)}</Text>
-        <Button title="Accept"></Button>
-        <Button title="Reject "></Button>
-      </View>
+  const handleAccept = (notificationId) => {
+    updateNotificationById(notificationId, { accepted: true }).then(
+      (notification) => {
+        setNotificationObj(notification);
+      }
     );
+  };
+
+  console.log(notificationObj);
+
+  if (notification.type === "join") {
+    if (notificationObj.accepted === false) {
+      return (
+        <View>
+          <Text>
+            {notificationObj.author} wants to join {notificationObj.choir}
+          </Text>
+          <Text>{Date(notificationObj.date)}</Text>
+          <Button
+            title="Accept"
+            onPress={() => handleAccept(notification._id)}
+          ></Button>
+          <Button
+            title="Reject"
+            onPress={() => handleReject(notification._id)}
+          ></Button>
+        </View>
+      );
+    } else {
+      return <View></View>;
+    }
   }
 
   if (notification.type === "message") {
