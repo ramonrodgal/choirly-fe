04dc51c3bf34e655b1f9a5c9c02fbdb6f0cb98fb
Serena Abbott <serena.abbott@hotmail.co.uk>
2021-12-13 10:14:03 +0000

    Add template for create message screen

---
+++ App/Screens/CreateMessageScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function CreateMessageScreen() {
+  return (
+    <View>
+      <Text>create message</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
