9af354040f1844ba04c8d7366b224935d4f81879
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-12-14 10:31:36 +0000

    Create notification for each member of a choir group when post a message

--- App/Screens/CreateMessageScreen.js
+++ App/Screens/CreateMessageScreen.js
@@ -1,29 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
-import { postMessage } from "../utils/api";
+import {
+  postMessage,
+  getChoirById,
+  postNotificationByUsername,
+} from "../utils/api";
 import { auth } from "../../firebase";
 import { useForm, Controller } from "react-hook-form";
 
+//WE SHOULD PASS THE ID OF THE GROUP
 export default function CreateMessageScreen() {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
 
+  const [members, setMembers] = useState([]);
+  const [choirName, setChoirName] = useState("");
+  const [leader, setLeader] = useState("");
+
+  const choir_id = "61b0c4c065064fdfb889a148"; //HARDCODED
+
   const handlePostMessage = () => {
     const body = {
-      choir: "African Children's Choir", //HARDCODED
+      choir: choirName,
       title: title,
       body: text,
-      author: auth.currentUser.email,
+      author: leader,
     };
     postMessage(body)
       .then((message) => {
         //RETURN TO PREVIUS PAGE
+
+        members.forEach((username) => {
+          const body = {
+            username: username,
+            choir: choirName,
+            type: "message",
+            message: text,
+            author: leader,
+          };
+
+          postNotificationByUsername(username, body)
+            .then((notification) => {
+              console.log(notification);
+            })
+            .catch((err) => {
+              console.log(err.response);
+            });
+        });
         console.log(message);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  };
+
+  useEffect(() => {
+    getChoirById(choir_id)
+      .then((choir) => {
+        setMembers(choir.members);
+        setChoirName(choir.name);
+        setLeader(choir.leader);
       })
       .catch((err) => {
         console.log(err);
       });
-  };
+  }, []);
 
   return (
     <View>
