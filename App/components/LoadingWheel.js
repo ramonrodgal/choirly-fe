e6a0ad3088d8c8ccb5d1a997ed3abd36f3e80d5a
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-12-21 18:36:14 +0000

    Change marginTop Value

--- App/components/LoadingWheel.js
+++ App/components/LoadingWheel.js
@@ -14,7 +14,7 @@ export default function LoadingWheel() {
 
 const styles = StyleSheet.create({
   loading: {
-    marginTop: 200,
+    marginTop: 50,
     width: 100,
     height: 100,
     alignSelf: "center",
