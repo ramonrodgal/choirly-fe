9ce854654f15dd1888aa0dbfa119bebe171fe4d9
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-12-21 18:03:04 +0000

    Create LoadingWheel component

---
+++ App/components/LoadingWhell.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+
+export default function LoadingWhell() {
+  return (
+    <Image
+      style={styles.loading}
+      source={{
+        uri: "https://www.teahub.io/photos/full/226-2267889_animated-circle-gif-transparent.gif",
+      }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  loading: {
+    marginTop: 200,
+    width: 100,
+    height: 100,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
+});
